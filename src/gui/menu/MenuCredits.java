03b560e11e09fda8332edcce3a46b5595f884a68
Taker314 <minecrafty512@gmail.com>
2015-11-25 17:45:42 +0100

    Put some stuff into the credits menu

--- src/gui/menu/MenuCredits.java
+++ src/gui/menu/MenuCredits.java
@@ -1,5 +1,6 @@
 package gui.menu;
 
+import font.fontMeshCreator.GUIText;
 import gui.element.Button;
 import gui.element.GuiElement;
 import gui.handler.HandlerChangeMenu;
@@ -15,9 +16,12 @@ public class MenuCredits extends Menu
 	@Override
 	public void doMenu()
 	{
-		guiElements.add(new GuiElement(loader.loadTexture("texture/gui/banner"), new Vector2f(0, H - 128), new Vector2f(W, 128), this));
+		guiElements.add(new GuiElement(loader.loadTexture("texture/gui/banner_credits"), new Vector2f(0, H - 128), new Vector2f(W, 128), this));
 		guiElementsBackground.add(new GuiElement(loader.loadTexture("texture/gui/background"), new Vector2f(0, 0), new Vector2f(W, H), this));
 		guiElements.add(new Button(new Vector2f(200, 100), buttonSize, this).setText("menu.back", font, 1).setIcon(loader.loadTexture("texture/gui/icon_back"), guiElementsForeground).setClickHandler(new HandlerChangeMenu(MainMenu.class)));
+		
+		new GUIText("o!Taker (minecrafty512@gmail.com)", 2, font, new Vector2f(50, H - 150), 1, false).setColour(0, 0.5F, 1);
+		new GUIText("o!KlötzchenTV", 2, font, new Vector2f(50, H - 200), 1, false).setColour(1, 0.5F, 0);
 		
 		Input input = new Input(Display.getHeight());
 		MouseHandler mouse = new MouseHandler(guiElements);
