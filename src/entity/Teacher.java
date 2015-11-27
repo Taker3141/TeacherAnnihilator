93ce9e0d2a58b8457f50c6fc3e94b4913d6843f6
Taker314 <minecrafty512@gmail.com>
2015-11-27 15:45:28 +0100

    Added health Improved raycaster Changed teacher hitbox size

--- src/entity/Teacher.java
+++ src/entity/Teacher.java
@@ -13,13 +13,14 @@ public class Teacher extends Entity
 
 	private float upwardsSpeed = 0;
 	private float terrainHeight = 0;
+	private int health = 100;
 	
 	public Teacher(TexturedModel model, Vector3f position, float rotX, float rotY, float rotZ, float scale, String name)
 	{
 		super(model, position, rotX, rotY, rotZ, scale);
 		this.name = name;
-		hitBox.size = new Vector3f(1, 2, 1);
-		hitBox.offset = new Vector3f(-0.5F, 0, -0.5F);
+		hitBox.size = new Vector3f(0.2F, 0.8F, 0.2F);
+		hitBox.offset = new Vector3f(-0.1F, 0, -0.1F);
 	}
 	
 	@Override
@@ -32,6 +33,20 @@ public class Teacher extends Entity
 		{
 			upwardsSpeed = 0;
 			position.y = terrainHeight;
+		}
+	}
+	
+	@Override
+	public void click()
+	{
+		if (health > 0)
+		{
+			health--;
+			System.out.println(name + ".health == " + health);
+		}
+		else
+		{
+			System.out.println(name + " is dead");
 		}
 	}
 }
--- src/raycasting/Raycaster.java
+++ src/raycasting/Raycaster.java
@@ -2,6 +2,7 @@ package raycasting;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.*;
 import renderer.MasterRenderer;
@@ -11,26 +12,36 @@ import entity.*;
 public class Raycaster
 {
 	public List<Collidable> list = new ArrayList<Collidable>();
-	private static final float step = 1F;
+	private static final float step = 0.1F;
+
+	private boolean isMouseDown = false;
 	
 	public void castRay(int MouseX, int MouseY, MasterRenderer master, Camera c)
 	{
-		Vector4f device = new Vector4f((((float)MouseX) / Display.getWidth() * 2) - 1, (((float)MouseY) / Display.getHeight() * 2) - 1, -1, 1);
-		Matrix4f inverseProjectionMatrix = Matrix4f.invert(master.getProjectionMatrix(), null);
-		Vector4f rayEye = Matrix4f.transform(inverseProjectionMatrix, device, null); rayEye.z = -1; rayEye.w = 0;
-		Matrix4f inverseViewMatrix = Matrix4f.invert(Maths.createViewMatrix(c), null);
-		Vector4f rayWorld4 = Matrix4f.transform(inverseViewMatrix, rayEye, null);
+		Vector4f rayEye = Matrix4f.transform(Matrix4f.invert(master.getProjectionMatrix(), null), new Vector4f((((float)MouseX) / Display.getWidth() * 2) - 1, (((float)MouseY) / Display.getHeight() * 2) - 1, -1, 1), null); rayEye.z = -1; rayEye.w = 0;
+		Vector4f rayWorld4 = Matrix4f.transform(Matrix4f.invert(Maths.createViewMatrix(c), null), rayEye, null);
 		Vector3f rayWorld = new Vector3f(rayWorld4.x, rayWorld4.y, rayWorld4.z);
 		rayWorld = rayWorld.normalise(rayWorld);
 		
 		Vector3f rayStep = new Vector3f(rayWorld.x * step, rayWorld.y * step, rayWorld.z * step);
 		Vector3f vec = new Vector3f(c.getPosition());
+		
+		boolean clickFlag = false;
+		if(!isMouseDown && Mouse.isButtonDown(0)) clickFlag = true;
+		isMouseDown = Mouse.isButtonDown(0);
+
+		endLoop:
 		for(int i = 0; i < 100; i++)
 		{
 			vec = Vector3f.add(vec, rayStep, vec);
 			for(Collidable e : list)
 			{
-				if(e.isInsideHitBox(vec)) e.hover();
+				if(e.isInsideHitBox(vec)) 
+				{
+					e.hover();
+					if(clickFlag) e.click();
+					break endLoop;
+				}
 			}
 		}
 	}
