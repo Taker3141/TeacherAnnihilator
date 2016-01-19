66d51aceb6feb1e18e1c5299ff3704e696d3ce29
Taker314 <minecrafty512@gmail.com>
2016-01-19 17:12:34 +0100

    Implemented collision with immovable objects

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -58,7 +58,7 @@ public class Entity implements ICollidable
 		for (ICollidable c : entityList)
 		{
 			if ((c instanceof BodyPart && ((BodyPart)c).isAttatched) || c == this) continue;
-			if (c.isInsideHitBox(hitBox)) 
+			if (isInsideHitBox(c.getHitBox())) 
 			{
 				if(c instanceof Movable && this instanceof Movable)
 				{
@@ -68,6 +68,10 @@ public class Entity implements ICollidable
 					Vector3f v2 = o2.v;
 					o1.v = new Vector3f((Vector3f)v2.scale(o2.mass).scale(1 / o1.mass));
 					o2.v = new Vector3f((Vector3f)v1.scale(o1.mass).scale(1 / o2.mass));
+				}
+				else if(!(c instanceof Movable) && this instanceof Movable)
+				{
+					((Movable)this).v = Vector3f.sub(position, ((Entity)c).position, null).normalise(null);
 				}
 //				return false;
 			}
