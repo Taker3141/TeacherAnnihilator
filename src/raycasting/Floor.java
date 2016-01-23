6b60fb0ed57cb87f18cf33c784c5b6fb076fa6c8
Taker314 <minecrafty512@gmail.com>
2016-01-23 13:08:38 +0100

    Added polygon vertices for floor

--- src/raycasting/Floor.java
+++ src/raycasting/Floor.java
@@ -1,19 +1,21 @@
 package raycasting;
 
+import org.lwjgl.util.vector.Vector2f;
 import org.lwjgl.util.vector.Vector3f;
 
 public class Floor implements IHitBox
 {
 	public Vector3f location, size, offset;
-	protected Vector3f[] polygon;
+	protected Vector2f[] polygon;
 	protected float height;
 
-	public Floor(Vector3f location, float height, Vector3f offset, Vector3f[] polygon)
+	public Floor(Vector3f location, float height, Vector3f offset, Vector2f[] polygon)
 	{
 		this.location = location;
 		this.height = height;
 		this.offset = offset;
 		this.polygon = polygon;
+		size = new Vector3f(80, 0.5F, 50);
 	}
 	
 	@Override
@@ -26,16 +28,6 @@ public class Floor implements IHitBox
 	@Override
 	public boolean isInside(IHitBox box)
 	{
-		Vector3f c1 = Vector3f.add(location, offset, null);
-		Vector3f c2 = Vector3f.add(c1, size, null);
-		if(box.isInside(c1)) return true;
-		if(box.isInside(new Vector3f(c1.x, c1.y, c2.z))) return true;
-		if(box.isInside(new Vector3f(c1.x, c2.y, c1.z))) return true;
-		if(box.isInside(new Vector3f(c1.x, c2.y, c2.z))) return true;
-		if(box.isInside(new Vector3f(c2.x, c1.y, c1.z))) return true;
-		if(box.isInside(new Vector3f(c2.x, c1.y, c2.z))) return true;
-		if(box.isInside(new Vector3f(c2.x, c2.y, c1.z))) return true;
-		if(box.isInside(c2)) return true;
 		return false;
 	}
 
