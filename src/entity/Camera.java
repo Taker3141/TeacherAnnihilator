df6d463a649de86c8d3c67ea3b67508bed6f36ec
Taker314 <minecrafty512@gmail.com>
2016-02-10 17:34:32 +0100

    Improved camera calculation

--- src/entity/Camera.java
+++ src/entity/Camera.java
@@ -1,6 +1,7 @@
 package entity;
 
 import java.util.Map.Entry;
+import main.MainGameLoop;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.util.vector.Vector3f;
@@ -25,6 +26,13 @@ public class Camera
 	{
 		calculatePitch();
 		calculateAngleAroundPlayer();
+		if(position.y - 0.5 < MainGameLoop.w.height(position.x, position.z)) 
+		{
+			float minHeight = MainGameLoop.w.height(position.x, position.z) + 0.5F;
+			float sin = (minHeight - player.position.y) / distanceFromPlayer;
+			if(sin > 1) sin = 1;
+			pitch = (float)Math.toDegrees(Math.asin(sin));
+		}
 		if (!isFirstPerson)
 		{
 			calculateZoom();
@@ -81,6 +89,7 @@ public class Camera
 		if (Mouse.isButtonDown(2))
 		{
 			float pitchChange = Mouse.getDY() * 0.1F;
+			if(position.y - 1 < MainGameLoop.w.height(position.x, position.z) && pitchChange > 0 && !isFirstPerson) return;
 			pitch -= pitchChange;
 			if (pitch < -90) pitch = -90;
 			if (pitch > 90) pitch = 90;
